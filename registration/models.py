b556cebce22fac1b8c62497676e5a8ef87eab855
Paul Ndambo <52633424+Paulndambo@users.noreply.github.com>
2024-02-19 07:48:07 +0300

    templates

--- registration/models.py
+++ registration/models.py
@@ -5,8 +5,13 @@ from django.db import models
 class Student(models.Model):
     name = models.CharField(max_length = 100)
     student_id = models.CharField(max_length = 20, unique =True)
+    registration_number = models.CharField(max_length=255, null=True)
+    id_number = models.CharField(max_length=255, null=True)
+    course = models.CharField(max_length=255, null=True)
     grade = models.CharField(max_length = 10)
     contact_info = models.CharField(max_length = 100)
+    phone_number = models.CharField(max_length=255, null=True)
+    email = models.EmailField(null=True)
     
     def __str__(self): 
         return self.name
